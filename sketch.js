2b005a27abd0c66b68575f044b112d998e485bcd
MeljoMichael <88435968+MeljoMichael@users.noreply.github.com>
2021-08-19 14:45:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+
+  box =  createSprite (200,200,30,30);
+
+  background("black");
+}
+
+function draw() 
+{
+
+  if (keyIsDown (RIGHT_ARROW)) {
+    box.x = box.x+5;
+
+    background('lightPink');
+  }
+
+
+  if (keyIsDown (LEFT_ARROW)) {
+    box.x = box.x-5;
+
+    background('lightBlue');
+  }
+
+  if (keyIsDown (UP_ARROW)) {
+    box.y = box.y-5;
+
+    background('lightYellow');
+  }
+
+
+  if (keyIsDown (DOWN_ARROW)) {
+    box.y = box.y+5;
+
+    background('lightGreen');
+  }
+  drawSprites();
+}
+
+
+
+
